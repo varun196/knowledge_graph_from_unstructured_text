6785fdd84c3972e135079ee95a5c5042c3408ccb
Rajat Dange <rajatdags97@gmail.com>
2019-05-03 16:47:17 -0400

    final improvements and dynamic file processing

--- create_structured_csv.py
+++ create_structured_csv.py
@@ -1,36 +1,45 @@
 import pickle
 import pandas as pd
 import os
+import glob
 
 
 def main():
-    with open('named_entities.pickle','rb') as f:
-        entities = pickle.load(f)
+
+    pickles = []
+    for file in glob.glob(os.getcwd() + "/data/output/ner/*.pickle"):
+        pickles.append(file)
+
+    for file in pickles:
+        with open(file,'rb') as f:
+            entities = pickle.load(f)
     
-    entity_set = set(entities.keys())
-    final_list = []
+        entity_set = set(entities.keys())
+        final_list = []
+        curr_dir = os.getcwd()
+        df = pd.read_csv(curr_dir +"/stanford-openie/corpus/input.txt-out.csv")
 
-    curr_dir = os.getcwd()
-    df = pd.read_csv(curr_dir +"stanford-openie/corpus/input.txt-out.csv")
+        triplet = set()
+        for i,j in df.iterrows():
+            j[0] = j[0].strip()
+            if j[0] in entity_set:
+                added = False
+                e2_sentence = j[2].split(' ')
+                for entity in e2_sentence:
+                    if entity in entity_set:
+                        _ = (entities[j[0]], str(j[0]), str(j[1]) ,entities[entity], str(j[2]) )
+                        triplet.add(_)
+                        added = True
+                if not added:
+                    _ = (entities[j[0]], str(j[0]), str(j[1]) ,'O', str(j[2]) )
+                    triplet.add(_)
 
-    triplet = []
-    for i,j in df.iterrows():
-        j[0] = j[0].strip()
-        if j[0] in entity_set:
-            added = False
-            for entity in j[2]:
-                if entity in entity_set:
-                    _ = (entities[j[0]], str(j[0]), str(j[1]) ,entity, str(j[2]) )
-                    triplet.append(_)
-                    added = True
-            if not added:
-                _ = (entities[j[0]], str(j[0]), str(j[1]) ,'OBJ', str(j[2]) )
-                triplet.append(_)
+        processed_pd = pd.DataFrame(list(triplet),columns=['Type','Entity 1','Relationship','Type', 'Entity2'])
+        processed_pd.to_csv('./data/result/processed' + file.split("/")[-1].split(".")[0] + '.csv', encoding='utf-8', index=False)
 
-    processed_pd = pd.DataFrame(triplet)
-    processed_pd.to_csv('processed.csv', encoding='utf-8', index=False)
+        print("Processed " + file.split("/")[-1])
 
-    print("File processed and saved")
+    print("Files processed and saved")
 
 if __name__ == '__main__':
     main()
--- relation_extractor.py
+++ relation_extractor.py
@@ -1,23 +1,23 @@
 import os
 import subprocess
+import glob
 import pandas as pd
 
 def Stanford_Relation_Extractor():
 
     
     print('Relation Extraction Started')
-    files = ['input.txt']
-    
-    for f in files:
-        
-        print(f)
-        os.chdir('/Users/rajat/Desktop/Knowledge_Graph/stanford-openie')
 
-        p = subprocess.Popen(['./process_large_corpus.sh','corpus/'+f,'corpus/' + f + '-out.csv'], stdout=subprocess.PIPE)
+    files = ['input.txt']
+    for f in glob.glob(os.getcwd() + "/data/output/kg/*.txt"):        
+        print("Extracting relations for " + f.split("/")[-1])
+        current_directory = os.getcwd()
+        os.chdir(current_directory + '/stanford-openie')
+
+        p = subprocess.Popen(['./process_large_corpus.sh',f,f + '-out.csv'], stdout=subprocess.PIPE)
 
         output, err = p.communicate()
-
-        
+   
 
     print('Relation Extraction Completed')
 
